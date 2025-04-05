9672577ac45e8bfa1b1861e9b2b6bdb7719edb74
mjFallah <mjfallah73@gmail.com>
2025-04-05 23:43:00 +0330

    add  SQL data queries with Drizzle ORM for customers and tickets

---
+++ src/app/(rs)/customers/form/page.tsx
@@ -0,0 +1,39 @@
+import { BackButton } from "@/components/back-button";
+
+import { getCustomer } from "@/lib/queries/getCustomers";
+
+import * as Sentry from "@sentry/nextjs";
+
+export default async function CustomerFormPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  try {
+    const { customerId } = await searchParams;
+
+    //Edit cutomer form
+    if (customerId) {
+      const customer = await getCustomer(parseInt(customerId));
+      if (!customer) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer ID #{customerId} not found
+            </h2>
+            <BackButton variant="default" title="Go Back" />
+          </>
+        );
+      }
+      console.log(customer);
+      // put customer form component
+    } else {
+      // new customer form component
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      Sentry.captureException(error);
+      throw error;
+    }
+  }
+}
---
+++ src/app/(rs)/tickets/form/page.tsx
@@ -0,0 +1,75 @@
+import { BackButton } from "@/components/back-button";
+
+import { getCustomer } from "@/lib/queries/getCustomers";
+import { getTickets } from "@/lib/queries/getTicket";
+
+import * as Sentry from "@sentry/nextjs";
+
+export default async function TicketFormPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  try {
+    const { customerId, ticketId } = await searchParams;
+
+    if (!customerId && !ticketId) {
+      return (
+        <>
+          <h2 className="text-2xl mb-2">
+            Ticket ID or Customer ID required to load ticket form
+          </h2>
+          <BackButton variant="default" title="Go Back" />
+        </>
+      );
+    }
+    // New Ticket Form
+    if (customerId) {
+      const customer = await getCustomer(parseInt(customerId));
+      if (!customer) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer ID #{customerId} not found
+            </h2>
+            <BackButton variant="default" title="Go Back" />
+          </>
+        );
+      }
+      if (!customer.active) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">
+              Customer ID #{customerId} is not active.
+            </h2>
+            <BackButton variant="default" title="Go Back" />
+          </>
+        );
+      }
+      // return ticket form
+      console.log(customer);
+    }
+    // Edit ticket form
+    if (ticketId) {
+      const ticket = await getTickets(parseInt(ticketId));
+      if (!ticket) {
+        return (
+          <>
+            <h2 className="text-2xl mb-2">Ticket ID #{ticketId} not found</h2>
+            <BackButton variant="default" title="Go Back" />
+          </>
+        );
+      }
+      const customer = await getCustomer(ticket.customerId);
+
+      //return ticket form
+      console.log("ticket :", ticket);
+      console.log("customer :", customer);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      Sentry.captureException(error);
+      throw error;
+    }
+  }
+}
