41d8f4346697be21b2bcdc844dd55eea1e6aa019
mjFallah <mjfallah73@gmail.com>
2025-04-13 22:47:14 +0330

    add search ticket functionality to tickets page

--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,7 +1,35 @@
+import TicketSearch from "@/app/(rs)/tickets/ticket-search";
+
+import { getTicketSearchResults } from "@/lib/queries/get-ticket-search-result";
+import { getOpenTickets } from "@/lib/queries/get-open-tickets";
+
 export const metadata = {
-  title: "Tickets",
+  title: "Ticket Search",
 };
 
-export default function Tickets() {
-  return <h2>Tickets Page</h2>;
+export default async function Tickets({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) {
+    const results = await getOpenTickets();
+    return (
+      <>
+        <TicketSearch />
+        <p>{JSON.stringify(results)}</p>
+      </>
+    );
+  }
+
+  const results = await getTicketSearchResults(searchText);
+
+  return (
+    <>
+      <TicketSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
