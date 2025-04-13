9765dbd574111f3e786dc156a30938bcc937f656
mjFallah <mjfallah73@gmail.com>
2025-04-13 22:47:02 +0330

    add search customer functionality to customers page

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,26 @@
+import CustomerSearch from "@/app/(rs)/customers/customer-search";
+
+import { getCustomerSearchResults } from "@/lib/queries/get-customer-search-result";
+
 export const metadata = {
-  title: "Customers",
+  title: "Customer Search",
 };
 
-export default function Customers() {
-  return <h2>Customers Page</h2>;
+export default async function Customers({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { searchText } = await searchParams;
+
+  if (!searchText) return <CustomerSearch />;
+
+  const results = await getCustomerSearchResults(searchText);
+
+  return (
+    <>
+      <CustomerSearch />
+      <p>{JSON.stringify(results)}</p>
+    </>
+  );
 }
