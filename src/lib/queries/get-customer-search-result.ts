5d72af2b2b89728d73cf039aaabdbb55856c86f4
mjFallah <mjfallah73@gmail.com>
2025-04-17 21:53:16 +0330

    add soring by last name to get-customer-search-result query

--- src/lib/queries/get-customer-search-result.ts
+++ src/lib/queries/get-customer-search-result.ts
@@ -18,6 +18,7 @@ export async function getCustomerSearchResults(searchText: string) {
           customers.lastName
         })) LIKE ${`%${searchText.toLowerCase().replace(" ", "%")}%`}`
       )
-    );
+    )
+    .orderBy(customers.lastName);
   return result;
 }
