8485edf874ca5c23742ea49da2a3948d1ebb5cd1
mjFallah <mjfallah73@gmail.com>
2025-04-18 19:02:33 +0330

    handle duplicate entries error in safe-action

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,6 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
 import { z } from "zod";
 import * as Sentry from "@sentry/nextjs";
+import { NeonDbError } from "@neondatabase/serverless";
 
 export const actionClient = createSafeActionClient({
   defineMetadataSchema() {
@@ -10,6 +11,12 @@ export const actionClient = createSafeActionClient({
   },
   handleServerError(error, utils) {
     const { clientInput, metadata } = utils;
+    if (error.constructor.name === "NeonDbError") {
+      const { code, detail } = error as NeonDbError;
+      if (code === "23505") {
+        return `Unique entry required. ${detail}`;
+      }
+    }
     Sentry.captureException(error, (scope) => {
       scope.clear();
       scope.setContext("serverError", { message: error.message });
